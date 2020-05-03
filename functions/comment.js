07055f1e7ecff1258cdb7c8c6508c45d62116f24
Andrew <healeycodes@gmail.com>
2020-05-03 21:59:17 +0100

    Testing getting file content

--- functions/comment.js
+++ functions/comment.js
@@ -5,15 +5,22 @@ const repo = process.env.GITHUB_REPO
 const user = process.env.GITHUB_USER
 
 exports.handler = async function (event, context, callback) {
-  const text = await fetch(
-    "https://api.github.com/repos/" + user + "/" + repo + "/contents/README.md",
-    {
-      headers: {
-        Authorization:
-          "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
-      },
-    }
-  ).then(res => res.text())
+  const oldComments = Buffer.from(
+    await fetch(
+      "https://api.github.com/repos/" +
+        user +
+        "/" +
+        repo +
+        "/contents/README.md",
+      {
+        headers: {
+          Authorization:
+            "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
+        },
+      }
+    ).then(res => res.text()),
+    "base64"
+  ).toString("utf-8")
 
   callback(null, {
     statusCode: 200,
