c5aeeffa5d611d86f8cf4e0c40752e0dcabb812a
Andrew <healeycodes@gmail.com>
2020-05-04 19:23:11 +0100

    Add PUT method for appending comments

--- functions/comment.js
+++ functions/comment.js
@@ -3,24 +3,48 @@ const fetch = require("node-fetch")
 const auth = process.env.GITHUB_PAT_TOKEN
 const repo = process.env.GITHUB_REPO
 const user = process.env.GITHUB_USER
+const api =
+  "https://api.github.com/repos/" +
+  user +
+  "/" +
+  repo +
+  "/contents/src/comments.json"
 
 exports.handler = async function (event, context, callback) {
-  const oldComments = Buffer.from(
-    await fetch(
-      "https://api.github.com/repos/" +
-        user +
-        "/" +
-        repo +
-        "/contents/README.md",
-      {
-        headers: {
-          Authorization:
-            "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
-        },
-      }
-    ).then(res => res.text()),
-    "base64"
-  ).toString("utf-8")
+  const existingFile = JSON.parse(
+    await fetch(api, {
+      headers: {
+        Authorization:
+          "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
+      },
+    }).then(res => res.text())
+  )
+
+  let comments = Buffer.from(existingFile.content, "base64").toString("utf-8")
+
+  const newComment = JSON.parse(event.body)
+  comments.push({
+    author: newComment.author,
+    email: newComment.email,
+    message: newComment.message,
+    date: Date.now(),
+  })
+
+  await fetch(api, {
+    method: "PUT",
+    headers: {
+      Authorization:
+        "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
+    },
+    body: JSON.stringify({
+      message: "New comment on " + new Date().toDateString(),
+      committer: {
+        name: "Netlify Serverless Function",
+      },
+      content: Buffer(comments).toString("base64"),
+      sha: existingFile.sha,
+    }),
+  })
 
   callback(null, {
     statusCode: 200,
