8e23b5595a3b77a30db7e3dac5b5575af6fa5ff4
Andrew <healeycodes@gmail.com>
2020-05-06 20:10:23 +0100

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# gatsby-comments
+# gatsby-serverless-comments
+
+This is a proof-of-concept for using Netlify serverless functions to let users add comments to the source code of a static website.
+
+1. 👩 User enters a comment and clicks submit.
+2. ⚙️ A serverless function receives the data.
+3. 🔧 The new comment is appended to `comments.json` via GitHub's Contents API.
+4. 🚧 A new commit triggers a Netlify CI build.
+5. ✔️ The new version of the website is deployed!
+
+The new comment is visible ~30 seconds ⏰ after the first click.
+
+<br>
+
+## Why?
+
+- Accept user comments and store them on GitHub. No database required.
+
+- Avoid using third-party plugin solutions that leak your user's data.
+
+<br>
+
+## Setup
+
+Set the following environmental variables on Netlify:
+
+- `GITHUB_PAT_TOKEN` to your GitHub personal access token.
+- `GITHUB_REPO` to the repository where this code is located.
+- `GITHUB_USER` your GitHub username.
+
+<br>
+
+Deploy!
--- functions/comment.js
+++ functions/comment.js
@@ -11,26 +11,29 @@ const api =
   "/contents/src/comments.json"
 
 exports.handler = async function (event, context, callback) {
-
   // Use the Contents API from GitHub
   // https://developer.github.com/v3/repos/contents/#get-contents
   const existingFile = JSON.parse(
     await fetch(api, {
       headers: {
+        // Pass some kind of authorization, I'm using a personal access token
         Authorization:
           "Basic " + Buffer.from(user + ":" + auth).toString("base64"),
       },
     }).then(res => res.text())
   )
 
-  // File content is stored in base64 encoding, let's parse that into utf-8 and then into an object
-  let comments = JSON.parse(Buffer.from(existingFile.content, "base64").toString("utf-8"))
+  // The file's content is stored in base64 encoding
+  // Decode that into utf-8 and then parse into an object
+  let comments = JSON.parse(
+    Buffer.from(existingFile.content, "base64").toString("utf-8")
+  )
 
-  // Here is the user submitted comment
-  // Perhaps here we would do some validation e.g. for security
+  // This is the user submitted comment
+  // Perhaps we would do some validation here (e.g. for security)
   const newComment = JSON.parse(event.body)
 
-  // Add the new comment to the existing comments
+  // Update the comments
   comments.push({
     author: newComment.author,
     email: newComment.email,
@@ -48,10 +51,10 @@ exports.handler = async function (event, context, callback) {
     body: JSON.stringify({
       message: "New comment on " + new Date().toDateString(),
 
-      // Let's turn that object back into a string and encoded it
+      // Turn that object back into a string and encoded it
       content: Buffer(JSON.stringify(comments)).toString("base64"),
 
-      // The blob SHA of the file being changed is required
+      // Required: the blob SHA of the file being changed is required
       sha: existingFile.sha,
     }),
   }).then(res => res.text())
